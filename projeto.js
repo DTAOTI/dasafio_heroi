bdb4105098d5263656a4d4f5d56ed1f99c1a1327
DTAOTI <danilotavares906@gmail.com>
2025-02-09 13:29:10 -0300

    projeto-heroi

---
+++ projeto.js
@@ -0,0 +1,35 @@
+let heroi = "Luffy"
+let valorDeRecompensa = "Nivel de Recomepensa"
+
+switch(valorDeRecompensa){
+    case "Ferro":
+      console.log("Valor 1.000")
+      break
+    case "Bronze":   
+       console.log("Valor 2.000")
+       break
+    case "Prata":
+       console.log("Valor 5.000")
+       break
+    case "Ouro":
+       console.log("Valor 7.000")
+       break
+    case "Platina":
+        console.log("Valor 8.000")
+        breck
+    case "Ascendente":
+        console.log ("Valor 10.000")
+        break
+    case "Imortal":
+        console.log ("Valor 15.000")
+        brack
+    case "Radiante":
+         console.log ("Valor 20.000")
+         break
+    
+    default:
+         console.log("Valor 100.000.0000.000")
+       
+}
+
+        console.log( valorDeRecompensa)
